45cbc7907e51fa8e1419bcc6da4ac1aec3182e62
Dev-JeanSantos <jeancbsan@gmail.com>
2023-09-14 11:41:17 -0300

    desafio nginx and node

---
+++ docker/desafios/nginx-node/node/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { queryPromise } = require('./queryPromise')
+
+
+async function createApp() {
+    const app = express()
+    const sqlTable = `CREATE TABLE IF NOT EXISTS people(id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))`;
+    await queryPromise.query(sqlTable)
+
+    const sql = `INSERT INTO people(name) values('Jean Santos')`
+    await queryPromise.query(sql)
+
+    app.get('/', async (req,res) => {
+        const selectPeople = `SELECT name FROM people`
+        const allPeople = await queryPromise.query(selectPeople)
+
+        const html = `<h1>Full Cycle</h1>\n
+        <ul>
+        ${allPeople.map(people => `<li>${people.name}</li>`).join('')}
+        </ul>`
+    
+        res.send(html)
+        
+    })
+    return app
+}
+
+module.exports = createApp
